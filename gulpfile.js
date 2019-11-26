e79331405be262d49e522be2ed007b52099e6e72
GenaBitu <developer@dedic.eu>
2019-11-26 11:37:29 +0100

    Added build script

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+/* eslint-env node */
+
+const gulp = require( 'gulp' );
+
+gulp.task( 'build:assets', function() {
+	return gulp.src( [ 'src/assets/license.txt', 'src/assets/readme.txt', 'src/assets/style.css' ] )
+		.pipe( gulp.dest( 'dist/' ) );
+} );
+
+gulp.task( 'build', gulp.parallel( 'build:assets' ) );
