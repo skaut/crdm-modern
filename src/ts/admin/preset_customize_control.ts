8c2b6f7ee04d1747dd3adb59365533ee2fa310d3
GenaBitu <developer@dedic.eu>
2019-11-29 17:19:08 +0100

    General flattening function

--- src/ts/admin/preset_customize_control.ts
+++ src/ts/admin/preset_customize_control.ts
@@ -1,1 +1,25 @@
-console.log( crdmModernPresetCustomizeControlLocalize );
+function flatten( input: object ): Record<string, string> {
+	function flattenInner( inputInner: object ): Record<string, string> {
+		const retInner: Record<string, string> = {};
+		$.each( inputInner, function( key, value ) {
+			if ( typeof ( value ) === 'object' ) {
+				$.each( flattenInner( value ), function( keyInner, valueInner ) {
+					retInner[ '[' + key + ']' + keyInner ] = valueInner;
+				} );
+			} else {
+				retInner[ '[' + key + ']' ] = value;
+			}
+		} );
+		return retInner;
+	}
+
+	const ret: Record<string, string> = {};
+	$.each( input, function( key, value ) {
+		$.each( flattenInner( value ), function( keyInner, valueInner ) {
+			ret[ key + keyInner ] = valueInner;
+		} );
+	} );
+	return ret;
+}
+
+console.log( flatten( crdmModernPresetCustomizeControlLocalize ) );
