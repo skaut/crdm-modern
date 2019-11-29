958ac5d1ab29bd9bb324a7cb829a5c73b5cd781e
GenaBitu <developer@dedic.eu>
2019-11-29 17:44:48 +0100

    Implemwented Preset_Customize_Control JS logic

--- src/ts/admin/preset_customize_control.ts
+++ src/ts/admin/preset_customize_control.ts
@@ -22,4 +22,31 @@ function flatten( input: object ): Record<string, string> {
 	return ret;
 }
 
-console.log( flatten( crdmModernPresetCustomizeControlLocalize ) );
+function applyPreset( control: any ): void {
+	const chosen = control.container.find( 'input[name=crdm_modern_preset]:checked' ).val();
+	if ( ! chosen ) {
+		return;
+	}
+
+	const preset = flatten( crdmModernPresetCustomizeControlLocalize[ chosen ] );
+	$.each( preset, function( key, value ) {
+		const setting = wp.customize( key );
+		if ( ! setting ) {
+			return;
+		}
+		setting.set( value );
+	} );
+}
+
+wp.customize.control(
+	'crdm_modern_preset',
+	function( control: any ) {
+		control.container.find( 'input[name=crdm_modern_preset]' ).change( function() {
+			control.container.find( '.button' ).prop( 'disabled', false );
+		} );
+
+		control.container.find( '.button' ).click( function() {
+			applyPreset( control );
+		} );
+	}
+);
