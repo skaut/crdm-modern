a299d1c74fefe08fd75ff59f65fd976f60dfc6a9
GenaBitu <developer@dedic.eu>
2020-02-17 09:27:37 +0100

    prettier re-style

--- gulpfile.ts
+++ gulpfile.ts
@@ -1,108 +1,146 @@
 /* eslint-env node */
 
-const gulp = require( 'gulp' );
+const gulp = require("gulp");
 
-const cleanCSS = require( 'gulp-clean-css' );
-const composer = require( 'gulp-uglify/composer' );
-const concat = require( 'gulp-concat' );
-const inject = require( 'gulp-inject-string' );
-const merge = require( 'merge-stream' );
-const rename = require( 'gulp-rename' );
-const ts = require( 'gulp-typescript' );
-const uglify = require( 'uglify-js' );
+const cleanCSS = require("gulp-clean-css");
+const composer = require("gulp-uglify/composer");
+const concat = require("gulp-concat");
+const inject = require("gulp-inject-string");
+const merge = require("merge-stream");
+const rename = require("gulp-rename");
+const ts = require("gulp-typescript");
+const uglify = require("uglify-js");
 
-const minify = composer( uglify, console );
+const minify = composer(uglify, console);
 
-gulp.task( 'build:css:main', function() {
-	return gulp.src( 'src/css/style.css' )
-		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
-		.pipe( gulp.dest( 'dist/' ) );
-} );
+gulp.task("build:css:main", function() {
+  return gulp
+    .src("src/css/style.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(gulp.dest("dist/"));
+});
 
-gulp.task( 'build:css:admin', function() {
-	return gulp.src( 'src/css/admin/**/*.css' )
-		.pipe( cleanCSS( { compatibility: 'ie8' } ) )
-		.pipe( rename( { suffix: '.min' } ) )
-		.pipe( gulp.dest( 'dist/admin/css/' ) );
-} );
+gulp.task("build:css:admin", function() {
+  return gulp
+    .src("src/css/admin/**/*.css")
+    .pipe(cleanCSS({ compatibility: "ie8" }))
+    .pipe(rename({ suffix: ".min" }))
+    .pipe(gulp.dest("dist/admin/css/"));
+});
 
-gulp.task( 'build:css', gulp.parallel( 'build:css:main', 'build:css:admin' ) );
+gulp.task("build:css", gulp.parallel("build:css:main", "build:css:admin"));
 
-gulp.task( 'build:deps:npm:dripicons', function() {
-	return gulp.src( [ 'node_modules/dripicons/webfont/webfont.css', 'node_modules/dripicons/webfont/fonts/*' ], { base: 'node_modules/dripicons/webfont' } )
-		.pipe( gulp.dest( 'dist/frontend/dripicons' ) );
-} );
+gulp.task("build:deps:npm:dripicons", function() {
+  return gulp
+    .src(
+      [
+        "node_modules/dripicons/webfont/webfont.css",
+        "node_modules/dripicons/webfont/fonts/*"
+      ],
+      { base: "node_modules/dripicons/webfont" }
+    )
+    .pipe(gulp.dest("dist/frontend/dripicons"));
+});
 
-gulp.task( 'build:deps:npm', gulp.parallel( 'build:deps:npm:dripicons' ) );
+gulp.task("build:deps:npm", gulp.parallel("build:deps:npm:dripicons"));
 
-gulp.task( 'build:deps', gulp.parallel( 'build:deps:npm' ) );
+gulp.task("build:deps", gulp.parallel("build:deps:npm"));
 
-gulp.task( 'build:jpg:frontend', function() {
-	return gulp.src( 'src/jpg/frontend/**/*.jpg' )
-		.pipe( gulp.dest( 'dist/frontend/' ) );
-} );
+gulp.task("build:jpg:frontend", function() {
+  return gulp
+    .src("src/jpg/frontend/**/*.jpg")
+    .pipe(gulp.dest("dist/frontend/"));
+});
 
-gulp.task( 'build:jpg', gulp.parallel( 'build:jpg:frontend' ) );
+gulp.task("build:jpg", gulp.parallel("build:jpg:frontend"));
 
-function bundle( name: string, sources: Array<string>, part: string, jQuery = false ): void {
-	const tsProject = ts.createProject( 'tsconfig.json' );
-	let ret = gulp.src( sources.concat( [ 'src/d.ts/**/*.d.ts' ] ) )
-		.pipe( tsProject() )
-		.js
-		.pipe( concat( name + '.min.js' ) );
-	if ( jQuery ) {
-		ret = ret.pipe( inject.prepend( 'jQuery( document ).ready( function( $ ) {\n' ) )
-			.pipe( inject.append( '} );\n' ) );
-	}
-	return ret.pipe( minify( { ie8: true } ) )
-		.pipe( gulp.dest( 'dist/' + part + '/js/' ) );
+function bundle(
+  name: string,
+  sources: Array<string>,
+  part: string,
+  jQuery = false
+): void {
+  const tsProject = ts.createProject("tsconfig.json");
+  let ret = gulp
+    .src(sources.concat(["src/d.ts/**/*.d.ts"]))
+    .pipe(tsProject())
+    .js.pipe(concat(name + ".min.js"));
+  if (jQuery) {
+    ret = ret
+      .pipe(inject.prepend("jQuery(document).ready( function( $) {\n"))
+      .pipe(inject.append("});\n"));
+  }
+  return ret
+    .pipe(minify({ ie8: true }))
+    .pipe(gulp.dest("dist/" + part + "/js/"));
 }
 
-gulp.task( 'build:js', function() {
-	return merge(
-		bundle( 'preset_customize_control', [ 'src/ts/admin/preset_customize_control.ts' ], 'admin', true ),
-		bundle( 'customizer', [ 'src/ts/admin/customizer.ts' ], 'admin', true ),
-		bundle( 'blog', [ 'src/ts/frontend/blog.ts' ], 'frontend', true )
-	);
-} );
+gulp.task("build:js", function() {
+  return merge(
+    bundle(
+      "preset_customize_control",
+      ["src/ts/admin/preset_customize_control.ts"],
+      "admin",
+      true
+    ),
+    bundle("customizer", ["src/ts/admin/customizer.ts"], "admin", true),
+    bundle("blog", ["src/ts/frontend/blog.ts"], "frontend", true)
+  );
+});
 
-gulp.task( 'build:php:root', function() {
-	return gulp.src( 'src/php/*.php' )
-		.pipe( gulp.dest( 'dist/' ) );
-} );
+gulp.task("build:php:root", function() {
+  return gulp.src("src/php/*.php").pipe(gulp.dest("dist/"));
+});
 
-gulp.task( 'build:php:admin', function() {
-	return gulp.src( 'src/php/admin/**/*.php' )
-		.pipe( gulp.dest( 'dist/admin/' ) );
-} );
+gulp.task("build:php:admin", function() {
+  return gulp.src("src/php/admin/**/*.php").pipe(gulp.dest("dist/admin/"));
+});
 
-gulp.task( 'build:php:frontend', function() {
-	return gulp.src( 'src/php/frontend/**/*.php' )
-		.pipe( gulp.dest( 'dist/frontend/' ) );
-} );
+gulp.task("build:php:frontend", function() {
+  return gulp
+    .src("src/php/frontend/**/*.php")
+    .pipe(gulp.dest("dist/frontend/"));
+});
 
-gulp.task( 'build:php', gulp.parallel( 'build:php:root', 'build:php:admin', 'build:php:frontend' ) );
+gulp.task(
+  "build:php",
+  gulp.parallel("build:php:root", "build:php:admin", "build:php:frontend")
+);
 
-gulp.task( 'build:png:screenshot', function() {
-	return gulp.src( 'src/png/screenshot.png' )
-		.pipe( gulp.dest( 'dist/' ) );
-} );
+gulp.task("build:png:screenshot", function() {
+  return gulp.src("src/png/screenshot.png").pipe(gulp.dest("dist/"));
+});
 
-gulp.task( 'build:png:admin', function() {
-	return gulp.src( 'src/png/admin/**/*.png' )
-		.pipe( gulp.dest( 'dist/admin/' ) );
-} );
+gulp.task("build:png:admin", function() {
+  return gulp.src("src/png/admin/**/*.png").pipe(gulp.dest("dist/admin/"));
+});
 
-gulp.task( 'build:png:frontend', function() {
-	return gulp.src( 'src/png/frontend/**/*.png' )
-		.pipe( gulp.dest( 'dist/frontend/' ) );
-} );
+gulp.task("build:png:frontend", function() {
+  return gulp
+    .src("src/png/frontend/**/*.png")
+    .pipe(gulp.dest("dist/frontend/"));
+});
 
-gulp.task( 'build:png', gulp.parallel( 'build:png:screenshot', 'build:png:admin', 'build:png:frontend' ) );
+gulp.task(
+  "build:png",
+  gulp.parallel("build:png:screenshot", "build:png:admin", "build:png:frontend")
+);
 
-gulp.task( 'build:txt', function() {
-	return gulp.src( [ 'src/txt/license.txt', 'src/txt/readme.txt' ] )
-		.pipe( gulp.dest( 'dist/' ) );
-} );
+gulp.task("build:txt", function() {
+  return gulp
+    .src(["src/txt/license.txt", "src/txt/readme.txt"])
+    .pipe(gulp.dest("dist/"));
+});
 
-gulp.task( 'build', gulp.parallel( 'build:css', 'build:deps', 'build:jpg', 'build:js', 'build:php', 'build:png', 'build:txt' ) );
+gulp.task(
+  "build",
+  gulp.parallel(
+    "build:css",
+    "build:deps",
+    "build:jpg",
+    "build:js",
+    "build:php",
+    "build:png",
+    "build:txt"
+  )
+);
--- src/ts/frontend/blog.ts
+++ src/ts/frontend/blog.ts
@@ -1,27 +1,29 @@
 function isArchive(): boolean {
-	const articles = $( '.site-main article .inside-article' );
-	return articles.length !== 1 || articles.find( '.entry-summary' ).length !== 0;
+  const articles = $(".site-main article .inside-article");
+  return articles.length !== 1 || articles.find(".entry-summary").length !== 0;
 }
 
 function addExcerptBorders(): void {
-	if ( ! isArchive() ) {
-		return;
-	}
-	const articles = $( '.site-main article .inside-article' );
-	articles.addClass( 'crdm_modern_excerpt' );
+  if (!isArchive()) {
+    return;
+  }
+  const articles = $(".site-main article .inside-article");
+  articles.addClass("crdm_modern_excerpt");
 }
 
 function addExcerptClickability(): void {
-	if ( ! isArchive() ) {
-		return;
-	}
-	const articles = $( '.site-main article .inside-article' );
-	articles.each( function( _, article ) {
-		const href = $( article ).find( '.entry-title a' ).attr( 'href' )!;
-		$( article ).click( function() {
-			location.href = href;
-		} );
-	} );
+  if (!isArchive()) {
+    return;
+  }
+  const articles = $(".site-main article .inside-article");
+  articles.each(function(_, article) {
+    const href = $(article)
+      .find(".entry-title a")
+      .attr("href")!;
+    $(article).click(function() {
+      location.href = href;
+    });
+  });
 }
 
 addExcerptBorders();
