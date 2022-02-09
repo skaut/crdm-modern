3106d6646d85bf0ba50a179d5ea54d8abca3a889
Marek Dědič <developer@dedic.eu>
2022-02-09 15:08:55 +0100

    PHPStan passing at level 8

--- src/php/frontend/class-title-widget.php
+++ src/php/frontend/class-title-widget.php
@@ -46,12 +46,16 @@ class Title_Widget extends \WP_Widget {
 		$css->add_property( 'margin-left', strval( absint( $gp_settings['logo_width'] ) + 20 ), false, 'px' );
 
 		$css->set_selector( '.crdm-modern-title-widget-title' );
-		$css->add_property( 'font-family', 'inherit' !== $gp_settings['font_site_title'] ? $site_title_family : null );
+		if ( 'inherit' !== $gp_settings['font_site_title'] ) {
+			$css->add_property( 'font-family', $site_title_family );
+		}
 		$css->add_property( 'font-size', strval( absint( $gp_settings['site_title_font_size'] ) ), false, 'px' );
 		$css->add_property( 'font-weight', strval( absint( $gp_settings['site_title_font_weight'] ) ), false );
 
 		$css->set_selector( '.crdm-modern-title-widget-tagline' );
-		$css->add_property( 'font-family', 'inherit' !== $gp_settings['font_site_tagline'] ? $site_tagline_family : null );
+		if ( 'inherit' !== $gp_settings['font_site_tagline'] ) {
+			$css->add_property( 'font-family', $site_tagline_family );
+		}
 		$css->add_property( 'font-size', strval( absint( $gp_settings['site_tagline_font_size'] ) ), false, 'px' );
 		$css->add_property( 'font-weight', strval( absint( $gp_settings['site_tagline_font_weight'] ) ), false );
 
