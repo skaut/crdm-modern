d47b731c2920ae9cfcc6f634e4e790f029419b94
Marek Dědič <developer@dedic.eu>
2022-04-26 10:11:53 +0200

    Better stubs for GenratePress_Pro_Range_Slider_Control

--- .phan/stubs/GeneratePress/class-generatepress-pro-range-slider-control.php
+++ .phan/stubs/GeneratePress/class-generatepress-pro-range-slider-control.php
@@ -11,4 +11,28 @@
 /**
  */
 class GeneratePress_Pro_Range_Slider_Control extends WP_Customize_Control {
+	/**
+	 * @param WP_Customize_Manager                                                                                                                                                                                                                                                                                                                                                                        $manager
+	 * @param string                                                                                                                                                                                                                                                                                                                                                                                      $id
+	 * @param array{instance_number?: int, manager?: WP_Customize_Manager, id?: string, settings?: array<int|string, string>, setting?: string, capability?: string, priority?: int, section?: string, label?: string, description?: string, choices?: array<string, array{min: string, max: string, step: string, edit: boolean, unit: string|false}>, input_attrs?: array<string, string>, allow_addition?: bool, json?: array<mixed>, type?: string, active_callback?: callable} $args {
+	 *     @type int                                                                                             $instance_number
+	 *     @type WP_Customize_Manager                                                                            $manager
+	 *     @type string                                                                                          $id
+	 *     @type array<int|string, string>                                                                       $settings
+	 *     @type string                                                                                          $setting
+	 *     @type string                                                                                          $capability
+	 *     @type int                                                                                             $priority
+	 *     @type string                                                                                          $section
+	 *     @type string                                                                                          $label
+	 *     @type string                                                                                          $description
+	 *     @type array<string, array{min: string, max: string, step: string, edit: boolean, unit: string|false}> $choices
+	 *     @type array<string, string>                                                                           $input_attrs
+	 *     @type bool                                                                                            $allow_addition
+	 *     @type array<mixed>                                                                                    $json
+	 *     @type string                                                                                          $type
+	 *     @type callable                                                                                        $active_callback
+	 * }
+	 */
+	public function __construct( $manager, $id, $args = array() ) {
+	}
 }
