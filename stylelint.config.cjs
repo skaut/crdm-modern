ecfcdfa0e2f3ee307d953734e4e7883966ace955
Marek Dědič <developer@dedic.eu>
2024-09-20 22:27:04 +0200

    Added stylelint-config-standard

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -2,9 +2,13 @@
 
 /** @type {import('stylelint').Config} */
 module.exports = {
-	extends: '@wordpress/stylelint-config/stylistic',
+	extends: [
+		'stylelint-config-standard',
+		'@wordpress/stylelint-config/stylistic',
+	],
 	plugins: ['stylelint-no-unsupported-browser-features'],
 	rules: {
+		'media-feature-range-notation': 'prefix',
 		'plugin/no-unsupported-browser-features': [
 			true,
 			{
